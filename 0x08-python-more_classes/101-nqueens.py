16385ec4a948437f29a6f117105b69aea5c90ed8
Shin-dara <101528551+Shin-dara@users.noreply.github.com>
2022-07-27 22:08:53 +0100

    Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,83 +1,142 @@
 #!/usr/bin/python3
-"""Solve the N queens problem"""
+"""Solves the N-queens puzzle.
 
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
 
+Example:
+    $ ./101-nqueens.py N
+
+N must be an integer greater than or equal to 4.
+
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
 import sys
 
 
-def checkQueen(queens, queen):
-    """Check if a queen is not attacking the others
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
+
+
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return (solution)
+
+
+def xout(board, row, col):
+    """X out spots on a chessboard.
+
+    All spots where non-attacking queens can no
+    longer be played are X-ed out.
 
     Args:
-        queens (list of tuple of int): queens placed so far
-        queen (tuple): queen to check
-
-    Returns:
-        bool: True if queen can be placed there, False otherwise
-
+        board (list): The current working chessboard.
+        row (int): The row where a queen was last played.
+        col (int): The column where a queen was last played.
     """
+    # X out all forward spots
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    # X out all backwards spots
+    for c in range(col - 1, -1, -1):
+        board[row][c] = "x"
+    # X out all spots below
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    # X out all spots above
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
+    # X out all spots diagonally down to the right
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally up to the left
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c]
+        c -= 1
+    # X out all spots diagonally up to the right
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally down to the left
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
 
-    for x, y in queens:
-        if y == queen[1]:
-            return False
-        if abs((y - queen[1]) / (x - queen[0])) == 1:
-            return False
-    return True
 
-
-def placeQueen(n, queens, solutions):
-    """Try to place the next queen on the board
+def recursive_solve(board, row, queens, solutions):
+    """Recursively solve an N-queens puzzle.
 
     Args:
-        n (int): number of queens that need to be placed
-        queens (list of tuple of int): queens placed so far
-        solutions (list of list of list of int): queen positions that work
-
-    """
-
-    if len(queens) == n:
-        solutions.append([list(q) for q in queens])
-        return
-    x = len(queens)
-    for y in range(n):
-        queen = (x, y)
-        if checkQueen(queens, queen):
-            queens.append(queen)
-            placeQueen(n, queens, solutions)
-            queens.pop()
-
-
-def validate_args():
-    """Validate the N queens program's command-line arguments
-
+        board (list): The current working chessboard.
+        row (int): The current working row.
+        queens (int): The current number of placed queens.
+        solutions (list): A list of lists of solutions.
     Returns:
-        int: first command-line argument
-
+        solutions
     """
+    if queens == len(board):
+        solutions.append(get_solution(board))
+        return (solutions)
 
+    for c in range(len(board)):
+        if board[row][c] == " ":
+            tmp_board = board_deepcopy(board)
+            tmp_board[row][c] = "Q"
+            xout(tmp_board, row, c)
+            solutions = recursive_solve(tmp_board, row + 1,
+                                        queens + 1, solutions)
+
+    return (solutions)
+
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('Usage: nqueens N')
+        print("Usage: nqueens N")
         sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print('N must be a number')
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
         sys.exit(1)
-    if n < 4:
-        print('N must be at least 4')
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
         sys.exit(1)
-    return n
 
-
-def main():
-    """Entry point to N queens program"""
-
-    n = validate_args()
-    queens = []
-    solutions = []
-    placeQueen(n, queens, solutions)
-    print('\n'.join(str(s) for s in solutions))
-
-
-if __name__ == '__main__':
-    main()
+    board = init_board(int(sys.argv[1]))
+    solutions = recursive_solve(board, 0, 0, [])
+    for sol in solutions:
+        print(sol)
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,151 +1,114 @@
 #!/usr/bin/python3
-"""Class that defines a rectangle"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """A rectangle with size, position, and methods to calculate more"""
+    """Represent a rectangle.
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
+    """
 
     number_of_instances = 0
-    """Tracks the number of instances that still exist"""
-
-    print_symbol = '#'
-    """The symbol used when drawing the string representation of rectangles"""
-
-    def __del__(self):
-        """Print a message when this rectangle is deleted"""
-
-        print('Bye rectangle...')
-        Rectangle.number_of_instances -= 1
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """Initialize a rectangle with optional size
+        """Initialize a new Rectangle.
 
         Args:
-            height (int): height of rectangle
-            width (int): width of rectangle
-
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
-
-        self.height = height
+        type(self).number_of_instances += 1
         self.width = width
-        Rectangle.number_of_instances += 1
-
-    def __repr__(self):
-        """Get a representation of this rectangle in Python code
-
-        Returns:
-            str: a representation of this rectangle that can be used by eval
-
-        """
-
-        return '{:s}({:d}, {:d})'.format(
-            type(self).__name__,
-            self.__width,
-            self.__height
-        )
-
-    def __str__(self):
-        """Draw this rectangle as a rectangle made of # characters
-
-        Returns:
-            str: string representation of this rectangle
-
-        """
-
-        if self.__height == 0 or self.__width == 0:
-            return ''
-        return '\n'.join(
-            str(self.print_symbol) * self.__width
-            for _ in range(self.__height)
-        )
-
-    @property
-    def height(self):
-        """The height of this rectangle
-
-        The setter ensures that this value is an integer and >= 0
-
-        """
-
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-        if value < 0:
-            raise TypeError('height must be >= 0')
-        self.__height = value
+        self.height = height
 
     @property
     def width(self):
-        """The width of this rectangle
-
-        The setter ensures that this value is an integer and >= 0
-
-        """
-
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError('width must be an integer')
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise TypeError('width must be >= 0')
+            raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """Get/set the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
     def area(self):
-        """Return this rectangle's area
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
 
-        Returns:
-            int: area
-
-        """
-
-        return self.__height * self.__width
+    def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """Return the largest of two rectangles
+        """Return the Rectangle with the greater area.
 
         Args:
-            rect_1 (Rectangle): left operand
-            rect_2 (Rectangle): right operand
-
-        Returns:
-            Rectangle: rect_1 if its area >= rect_2, rect_2 otherwise
-
+            rect_1 (Rectangle): The first Rectangle.
+            rect_2 (Rectangle): The second Rectangle.
+        Raises:
+            TypeError: If either of rect_1 or rect_2 is not a Rectangle.
         """
-
         if not isinstance(rect_1, Rectangle):
-            raise TypeError('rect_1 must be an instance of Rectangle')
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            raise TypeError('rect_2 must be an instance of Rectangle')
-        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
-
-    def perimeter(self):
-        """Return this rectangle's perimeter
-
-        Returns:
-            int: perimeter
-
-        """
-
-        if self.__height == 0 or self.__width == 0:
-            return 0
-        return self.__height * 2 + self.__width * 2
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return (rect_1)
+        return (rect_2)
 
     @classmethod
     def square(cls, size=0):
-        """Create a new Rectangle representing a square with given size
+        """Return a new Rectangle with width and height equal to size.
 
         Args:
-            size (int): length of sides of new square
-
-        Returns:
-            Rectangle: new Rectangle with given side lengths
-
+            size (int): The width and height of the new Rectangle.
         """
+        return (cls(size, size))
 
-        return cls(size, size)
+    def __str__(self):
+        """Return the printable representation of the Rectangle.
+
+        Represents the rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
+
+    def __repr__(self):
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
+
+    def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
